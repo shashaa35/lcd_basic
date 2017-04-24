7372cb17a3f4307e2b725beb2fb455b4da71e78a
Matt <email@example.com>
2017-04-24 18:29:05 +0000

    added inspec tests

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,14 +5,63 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
+packages = []
+
+%w(net-tools php-common).each do |item|
+  case os[:family]
+  when 'redhat'
+    packages << 'httpd'
+  when 'debian'
+    packages << 'apache2'
+  end
+    packages << item
+end
+
+packages.each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
   end
 end
 
+case os[:family]
+when 'redhat'
+  describe file('/usr/bin/php') do
+    it { should exist }
+    its('mode') { should cmp '00755' }
+    its('owner') { should eq 'root' }
+    its('group') { should eq 'root' }
+  end
+
+  describe service('httpd') do
+    it { should be_running }
+  end
+
+when 'debian'
+  describe file('/usr/bin/php7.0') do
+    it { should exist }
+    its('mode') { should cmp '00755' }
+    its('owner') { should eq 'root' }
+    its('group') { should eq 'root' }
+  end
+
+  describe service('apache2') do
+    it { should be_running }
+  end
+end
+
+describe group('developers') do
+  it { should exist }
+end
+
+describe user('webadmin') do
+  it { should exist }
+  its('group') { should eq 'developers' }
+end
+
 describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+  it { should be_listening }
+end
+
+describe command 'curl http://localhost' do
+  its('stdout') { should match(/Greetings, Planet Earth!/) }
 end
